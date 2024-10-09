ffe78cca5e064fe9a0e85127a2919286ad5799d8
Purity Wambua <puritywamb@gmail.com>
2024-10-09 12:06:37 +0300

    crud tests

--- server/app.py
+++ server/app.py
@@ -19,9 +19,14 @@ api = Api(app)
 
 class Plants(Resource):
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+    # def get(self):
+    #     plants = [plant.to_dict() for plant in Plant.query.all()]
+    #     return make_response(jsonify(plants), 200)
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
 
     def post(self):
         data = request.get_json()
@@ -44,8 +49,33 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        # return make_response(jsonify(plant), 200)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204  # No Content
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
